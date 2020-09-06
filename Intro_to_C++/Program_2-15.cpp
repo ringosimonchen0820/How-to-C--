7d950795f960ea386952a400746856245d992720
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-06 14:02:38 -0400

    Program 2-15

--- Intro_to_C++/Program_2-15.cpp
+++ Intro_to_C++/Program_2-15.cpp
@@ -3,3 +3,13 @@
 
 #include <iostream>
 #include <string> // Required for the string class. 
+using namespace std;
+
+int main()
+{
+    string movieTitle;
+
+    movieTitle = "Wheels of Fury";
+    cout << "My favorite movie is " << movieTitle << endl;
+    return 0;
+}
