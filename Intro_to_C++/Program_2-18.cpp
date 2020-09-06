c8fafcd4ce8e5dc24f08da79de0a9c267ee1e0b8
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-06 18:23:15 -0400

    Program 2-18

--- Intro_to_C++/Program_2-18.cpp
+++ Intro_to_C++/Program_2-18.cpp
@@ -1,2 +1,23 @@
 // This program determines the size of intergers, long intergers, and long doubles.
 //!   The sizeof operator may be used to determine the size of a data type on any system.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long double apple;
+
+    cout << "The size of an interger is " << sizeof(int);
+    cout << " bytes." << endl;
+
+    cout << "The size of a long interger is " << sizeof(long);
+    cout << " bytes." << endl;
+
+    cout << "An apple can be eaten in " << sizeof(apple);
+    cout << " bytes!" << endl;
+
+    return 0;
+
+}
