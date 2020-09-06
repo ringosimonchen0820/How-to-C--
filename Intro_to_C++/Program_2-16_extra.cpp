4520553c706c033fc54fcb77a8e530ec000a0712
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-06 16:09:49 -0400

    program 2-16

---
+++ Intro_to_C++/Program_2-16_extra.cpp
@@ -0,0 +1,16 @@
+// This program assigns floating-point values to interer variables.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    float f;
+    
+    f = 7.5;
+    i = f; // Assign 7 to i
+    cout << "i should be 7 ==> i = " << i << endl;
+    return 0;
+}
