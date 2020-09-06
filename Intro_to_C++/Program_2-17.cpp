cfa225a47a34d63bf4f3d2f1cd1b784aaa4b9587
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-06 18:09:37 -0400

    Program 2-17

---
+++ Intro_to_C++/Program_2-17.cpp
@@ -0,0 +1,17 @@
+// This program demonstrate boolean variables.
+    // Boolean variables are set to either true(1) or false(0)
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool boolValue;
+
+    boolValue = true;
+    cout << boolValue << endl; // output will be 1
+    boolValue = false;
+    cout << boolValue << endl; // output will be 0
+    return 0;
+}
