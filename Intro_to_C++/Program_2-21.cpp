4946c5f258d8315854a7e45a3af554a53a1d769c
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-06 23:07:37 -0400

    Program 2-21

---
+++ Intro_to_C++/Program_2-21.cpp
@@ -0,0 +1,30 @@
+// This program calculates hourly wages, including overtime.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double  regularWages,                   
+            basePayRate = 18.25,
+            regularHours = 40.0,
+            overtimeWages,
+            overtimePayRate = 27.78,
+            overtimeHours = 10,
+            totalWages;
+    
+    // Calculate the regular wages.
+    regularWages = basePayRate * regularHours;
+
+    // Calculate the overtime wages.
+    overtimeWages = overtimePayRate * overtimeHours;
+
+    // Calculate the total wages.
+    totalWages = regularWages + overtimeWages;
+
+    // Display the total wages.
+    cout << "Wages for this week are $" << totalWages << endl;
+
+    return 0;
+}
