282cf0a3d5406a1884318b10daa662cb9a5237f0
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-07 15:46:09 -0400

    Program 2-26 single line comments

---
+++ Intro_to_C++/Program_2-26.cpp
@@ -0,0 +1,18 @@
+// PROGRAM: PAYROLL.CPP
+// Written by hervert Dorfmann
+// This program calculates company payroll
+// Last modification: 8/20/2017
+//! This is just a demonstration on how single-line comments work 
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double payRate;     // Holds the hourly pay rate
+    double hours;       // Holds the hours worked
+    int employNumber;   // Holds the employee number
+
+    return 0;
+}
