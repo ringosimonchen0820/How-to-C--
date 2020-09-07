35dbb1690230292bfdee8525b4fcb0981245486b
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-07 16:07:13 -0400

    program 2-29 ---> bad programming style

---
+++ Intro_to_C++/Program_2-29.cpp
@@ -0,0 +1,13 @@
+/*
+!   Programming style refers to the way a programmer uses identifiers,
+!   spaces, tabs, blank linesm and punctuation characters to visually 
+!   arrange a program's source code.
+!   These are some, but not all, of the elements of programming style.
+*/
+
+
+#include <iostream>
+using namespace std; int main() {double shares=220.0;
+double avgPrice =14.67; cout<<" There were " << shares <<" shares sold at $" <<avgPrice<<" per share.\n";
+return 0;}
+
