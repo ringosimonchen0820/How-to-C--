2696a64a8f9c044fed290b6fb2033ed02bce71d2
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-08 13:37:36 -0400

    Program 3-1

--- Expressions_and_Interactivity/Program_3-1.cpp
+++ Expressions_and_Interactivity/Program_3-1.cpp
@@ -1,1 +1,22 @@
 // This program asks the user to enter the length and width of a rectangle. It calculates the rectangle's area and displays the value on the screen.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length, 
+        width, 
+        area;
+
+    cout << "This program calculates the area of a rectangle.\n";
+    cout << "What is the length of the rectangle? ";
+    cin >> length;
+    cout << "What is the width of the rectangel? ";
+    cin >> width;
+    area = length * width;
+    cout << "The area of the rectangle is " << area << "." << endl;
+
+    return 0;
+}
