02d44835d6a6f2a90c6ff3aa1bd153e5e4b721ad
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-13 21:28:49 -0400

    Program 3-11

---
+++ Expressions_and_Interactivity/Program_3-11.cpp
@@ -0,0 +1,21 @@
+// This program tracks the inventory of three widget stores that opened at the same time. Each store started with the same number of widgets in inventory. By substracting the number of widgets each store has sold from its inventory, the current inventory can be calculated.
+
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int begInv,
+        sold,
+        store1,
+        store2,
+        store3;
+
+
+    // Get the begining inventory for all the stores.
+
+}
