f6d07ae1a5d46faae3503c91ea5c84a9455adbdf
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-23 00:57:22 -0400

    program 4-5

---
+++ Making Decisions/Program_4-5.cpp
@@ -0,0 +1,34 @@
+// This program average 3 test scores. 
+// The if statement uses the = operator, but the == operator was intended.
+
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int score1, score2, score3; // To hold three test scores
+    double average;             // To hold the average score
+
+    // Get the three test scores
+    cout << "Enter 3 test scores and I will average them: ";
+    cin >> score1 >> score2 >> score3;
+
+    // Calculate and display the average score.
+    average = (score1 + score2 + score3) / 3.0;
+    cout << fixed << showpoint << setprecision(1);
+    cout << "Your average is " << average << endl;
+
+    /* 
+        Our intention is to congratulate the user
+        for having a perfect score. But, this doesn't work.
+    */
+    if (average = 100)                    //! WRONG! This is an assignment!
+        cout << "Congratulations! That's perfect score!\n";
+    
+    return 0;
+}
+
+//? Regardless of the average scorem this program will print the message congratulating the user on perfect score.
