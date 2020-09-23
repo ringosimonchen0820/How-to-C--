6396d6aa3069af134a398e3cbc73a99f00d0f4f9
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-22 20:42:26 -0400

    program 4-4

---
+++ Making Decisions/Program_4-4.cpp
@@ -0,0 +1,21 @@
+// This program demonstrates how floating-point round-off errirs can make equality operations unreliable.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a = 1.5;         // a is 1.5
+    double b = 1.5;         // b is 1.5
+
+    a += 0.0000000000000001;    // Add a little to a.
+    if (a == b)
+        cout << "Both a and b are the same.\n";
+    else
+    
+        cout << "A and b are not the same.\n";
+    
+    return 0;
+}
