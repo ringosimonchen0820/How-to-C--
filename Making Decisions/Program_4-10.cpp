bb2fbf87ad721aad42d64e7409ae5a6e21eaf22f
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-24 02:10:24 -0400

    Program 4-10

---
+++ Making Decisions/Program_4-10.cpp
@@ -0,0 +1,33 @@
+// This program demonstrates the nested if statement.
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char employed,      // Currently employed, Y or N
+         recentGrad;    // Recent graduate, Y or N
+    
+    // Is the user employed and a recent graduate?
+    cout << "Answer the following questions\n";
+    cout << "with either Y for Yes or N for no.\n";
+    cout << "Are you employed? ";
+    cin >> employed;
+    cout << "Have you graduated from college ";
+    cout << "in the past two years? ";
+    cin >> recentGrad;
+
+    // Determine the user;s loan qualifiactaions.
+    if (employed == 'Y')
+    {
+        if (recentGrad == 'Y') // Nested if
+        {
+            cout << "You qualify for the special ";
+            cout << "interest rate.\n";
+        }
+    }
+
+    return 0;
+}
