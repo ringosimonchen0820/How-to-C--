66189da3cd2c73397bdfe4d18a020bf65550f0a9
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-30 05:06:38 -0400

    program 4-22

---
+++ 03-Making Decisions/Program_4-23.cpp
@@ -0,0 +1,27 @@
+// The switch statement in this program tells the user something 
+//  he or she already knows: the data just entered!
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char choice;
+
+    cout << "Enter A, B, or C: ";
+    cin  >> choice;
+    switch (choice)
+    {
+        case 'A' : cout << "You entered A.\n";
+                   break;
+        case 'B' : cout << "You entered B.\n";
+                   break;
+        case 'C' : cout << "You entered B.\n";
+                   break;
+        default  : cout << "You did not enter A, B, or C!\n";
+    }
+
+    return 0;
+}
