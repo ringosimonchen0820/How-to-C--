829dc132d251f91bf168355a26bb19cc815e8f78
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-09-30 06:31:35 -0400

    program 4-29

---
+++ 03-Making Decisions/Program_4-29.cpp
@@ -0,0 +1,45 @@
+// This program demonstrates a variable defined in an inner block
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Constants for minimum income and years
+    const double MIN_INCOME = 35000.0;
+    const int MIN_YEARS = 5;
+
+    // Get the annual income.
+    cout << "What is your annual income? ";
+    double income;      // Variable definition , the scope of this variable starts from here
+    cin >> income;
+
+    // Determine the user's loan qualifications
+    if (income >= MIN_INCOME)
+    {
+        // Get the number of years at the current job.
+        cout << "How many years have you worked at "
+             << "your current job? ";
+        int years;       // Variable definition , the scope of this variable starts from here
+        cin >> years;
+
+        if (years > MIN_YEARS)
+            cout << "You qualify.\n";
+        else
+        {
+            cout << "You must have been employed for more than "
+                 << MIN_YEARS
+                 << " years to qualify.\n";
+        }
+    }   
+    else
+    {
+        cout << "You must earn at least $"
+             << MIN_INCOME
+             << " to qualify.\n";
+    }
+
+    return 0;
+}
