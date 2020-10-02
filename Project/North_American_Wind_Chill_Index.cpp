e14e9f0aa9982de13439a7dd8f8e69dff942888b
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-10-01 22:38:57 -0400

    project north american wind chill index

---
+++ Project/North_American_Wind_Chill_Index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Set up variables
+    double temp_wc;
+    int wind_speed = 5;
+    double temp_i = -5.0;
+
+    
+
+    return 0;
+}
