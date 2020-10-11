eefdd45eac8a1876befd4571e9684be2cfcfbaae
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-10-11 05:00:26 -0400

    activity 12

---
+++ Activity/activity_12.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void ulamSeq(int num);
+int main() 
+{
+    int n = 0;
+
+    // Input number from user
+    cout << "Enter number: ";
+    cin  >> n;
+
+    // Validate user input
+    while(n < 2) {
+        cout << "Number should be greater than 2, Enter again: ";
+        cin  >> n;
+    }
+
+    // Call the ulam sequence function
+    ulamSeq(n);
+
+    return 0;
+}
+
+// Create ulam sequence function
+void ulamSeq(int num) 
+{
+    cout << "Calculating ULAM sequence starting from " << num  << " : "<< endl;
+    cout << num;
+
+    do 
+    {
+    if(num % 2 == 0) // Checks for odd or even function
+        num = num / 2;
+
+    else
+    num = num * 3 + 1;
+        
+    cout << " " << num;
+    } while(num != 1);
+}
