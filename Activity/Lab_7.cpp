fb19244fdfb1e2a9506a1e16048015bec85d05a3
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-10-14 01:18:38 -0400

    lab 7

--- Activity/Lab_7.cpp
+++ Activity/Lab_7.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
+    // import files
     ifstream inf("FleetInput.txt");
 
+    // Create variables
     string  id;
     int     FLT_MAX ;
     float   miles,
