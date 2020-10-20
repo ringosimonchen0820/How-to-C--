7dc81c467119031f749b8c5ac1acd99eb1a15a77
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-10-20 00:26:37 -0400

    program 5-21

---
+++ 04-Loops_and_Files/Program_5-21.cpp
@@ -0,0 +1,39 @@
+// This program writes data to a file
+
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream outputFile;        // File stream object
+    int numberOfDays;           // Number of days of sales
+    double sales;               // Sales amount for a day
+
+    // Get the number of days.
+    cout << "For how many days do you have sales? ";
+    cin  >> numberOfDays;
+
+    // Open a dile named Sales.txt
+    outputFile.open("Sales.txt");
+
+    // Get the sales for each day and write it to the file.
+    for (int count = 1; count <= numberOfDays; count++)
+    {
+        // Get the sales for a day.
+        cout << "Enter the sales for day "
+             << count << ": ";
+        cin  >> sales;
+
+        // Write the sales to the file
+        outputFile << sales << endl;
+    }
+
+    // Close the file.
+    outputFile.close();
+    cout << "Data written to Sales.txt\n";
+
+    return 0;
+}
