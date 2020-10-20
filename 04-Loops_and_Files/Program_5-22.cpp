57f2c69cae777387e01dd9fb9636d371d38bdbb3
ringosimonchen0820 <54449798+ringosimonchen0820@users.noreply.github.com>
2020-10-20 00:40:32 -0400

    program 5-22

---
+++ 04-Loops_and_Files/Program_5-22.cpp
@@ -0,0 +1,27 @@
+// This program reads data from a file
+
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream inputFile;
+    int number;
+
+    // Open the file
+    inputFile.open("Numbers.txt");
+
+    // Read the numbers from the file and display them
+    while (inputFile >> number)
+    {
+        cout << number << endl;
+    }
+
+    // Close the file 
+    inputFile.close();
+
+    return 0;
+}
